142f184bf9d54a7c72f6ca607e4e711267e3c553
chethan-kumar-anand <chethan.anand93@gmail.com>
2022-08-31 23:49:35 +0530

    adding missing @Controller annotation

--- sample-mvc/src/main/java/com/springmvc/controller/RegistrationController.java
+++ sample-mvc/src/main/java/com/springmvc/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.springmvc.model.User;
 import com.springmvc.service.UserService;
 
+@Controller
 public class RegistrationController {
 
     @Autowired
